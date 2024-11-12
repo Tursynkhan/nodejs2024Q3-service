07800e6a97cc8c3064bd6fd951d44db1c39a4370
Tursynkhan <tursynkhan001@gmail.com>
2024-11-13 00:01:45 +0500

    add: favorites

---
+++ src/favorites/favorites.controller.ts
@@ -0,0 +1,43 @@
+// src/favorites/favorites.controller.ts
+import { Controller, Get, Post, Delete, Param, HttpCode } from '@nestjs/common';
+import { FavoritesService } from './favorites.service';
+
+@Controller('favs')
+export class FavoritesController {
+  constructor(private readonly favoritesService: FavoritesService) {}
+
+  @Get()
+  findAll() {
+    return this.favoritesService.findAll();
+  }
+
+  @Post('track/:id')
+  addTrack(@Param('id') id: string) {
+    this.favoritesService.addTrack(id);
+  }
+
+  @Delete('track/:id')
+  removeTrack(@Param('id') id: string) {
+    this.favoritesService.removeTrack(id);
+  }
+
+  @Post('album/:id')
+  addAlbum(@Param('id') id: string) {
+    this.favoritesService.addAlbum(id);
+  }
+
+  @Delete('album/:id')
+  removeAlbum(@Param('id') id: string) {
+    this.favoritesService.removeAlbum(id);
+  }
+
+  @Post('artist/:id')
+  addArtist(@Param('id') id: string) {
+    this.favoritesService.addArtist(id);
+  }
+
+  @Delete('artist/:id')
+  removeArtist(@Param('id') id: string) {
+    this.favoritesService.removeArtist(id);
+  }
+}
---
+++ src/favorites/favorites.service.ts
@@ -0,0 +1,137 @@
+// src/favorites/favorites.service.ts
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { Favorites } from './favorites.entity';
+import { isUUID } from 'uuid';
+import { ArtistService } from '../artist/artist.service';
+import { AlbumService } from '../album/album.service';
+import { TrackService } from '../track/track.service';
+
+@Injectable()
+export class FavoritesService {
+  private favorites: Favorites = {
+    artists: [],
+    albums: [],
+    tracks: [],
+  };
+
+  constructor(
+    private readonly artistService: ArtistService,
+    private readonly albumService: AlbumService,
+    private readonly trackService: TrackService,
+  ) {}
+
+  findAll() {
+    return {
+      artists: this.favorites.artists.map((id) =>
+        this.artistService.findOne(id),
+      ),
+      albums: this.favorites.albums.map((id) => this.albumService.findOne(id)),
+      tracks: this.favorites.tracks.map((id) => this.trackService.findOne(id)),
+    };
+  }
+
+  addTrack(id: string) {
+    if (!isUUID(id)) {
+      throw new BadRequestException('Invalid UUID');
+    }
+    try {
+      this.trackService.findOne(id);
+    } catch (error) {
+      throw new UnprocessableEntityException(
+        `Track with id ${id} does not exist`,
+      );
+    }
+    if (!this.favorites.tracks.includes(id)) {
+      this.favorites.tracks.push(id);
+    }
+  }
+
+  removeTrack(id: string) {
+    if (!isUUID(id)) {
+      throw new BadRequestException('Invalid UUID');
+    }
+    const index = this.favorites.tracks.indexOf(id);
+    if (index === -1) {
+      throw new NotFoundException('Track not found in favorites');
+    }
+    this.favorites.tracks.splice(index, 1);
+  }
+
+  addAlbum(id: string) {
+    if (!isUUID(id)) {
+      throw new BadRequestException('Invalid UUID');
+    }
+    try {
+      this.albumService.findOne(id);
+    } catch (error) {
+      throw new UnprocessableEntityException(
+        `Album with id ${id} does not exist`,
+      );
+    }
+    if (!this.favorites.albums.includes(id)) {
+      this.favorites.albums.push(id);
+    }
+  }
+
+  removeAlbum(id: string) {
+    if (!isUUID(id)) {
+      throw new BadRequestException('Invalid UUID');
+    }
+    const index = this.favorites.albums.indexOf(id);
+    if (index === -1) {
+      throw new NotFoundException('Album not found in favorites');
+    }
+    this.favorites.albums.splice(index, 1);
+  }
+
+  addArtist(id: string) {
+    if (!isUUID(id)) {
+      throw new BadRequestException('Invalid UUID');
+    }
+    try {
+      this.artistService.findOne(id);
+    } catch (error) {
+      throw new UnprocessableEntityException(
+        `Artist with id ${id} does not exist`,
+      );
+    }
+    if (!this.favorites.artists.includes(id)) {
+      this.favorites.artists.push(id);
+    }
+  }
+
+  removeArtist(id: string) {
+    if (!isUUID(id)) {
+      throw new BadRequestException('Invalid UUID');
+    }
+    const index = this.favorites.artists.indexOf(id);
+    if (index === -1) {
+      throw new NotFoundException('Artist not found in favorites');
+    }
+    this.favorites.artists.splice(index, 1);
+  }
+
+  // Methods to remove IDs when entities are deleted
+  removeTrackId(id: string) {
+    this.favorites.tracks = this.favorites.tracks.filter(
+      (trackId) => trackId !== id,
+    );
+  }
+
+  removeAlbumId(id: string) {
+    this.favorites.albums = this.favorites.albums.filter(
+      (albumId) => albumId !== id,
+    );
+  }
+
+  removeArtistId(id: string) {
+    this.favorites.artists = this.favorites.artists.filter(
+      (artistId) => artistId !== id,
+    );
+  }
+}
